21a98cfe050d72657421ee2812dafe5a7ca4d31d
rainbough <rainbough@gmail.com>
2014-05-21 20:11:19 -0500

    add new logo and styles

--- server/browser.js
+++ server/browser.js
@@ -1,14 +1,6 @@
-
-
 Meteor.methods({
-	browserCheck: function(){
-		var ie8 = false;
-		if(Bowser.msie && Bowser.version <= 8){
-			ie8 = true;
-		} else {
-			ie8 = false;
-		}
-		console.log(ie8);
-		return ie8;
+	layoutCheck: function(){
+		return bowser.msie && bowser.version <= 8;
+		
 	}
 });
